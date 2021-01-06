c2d6cc88d7a7b9ce4a435e5f51e4a40f98530832
robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-01-06 11:43:15 +0800

    註冊MaskService 呼叫MaskService

---
+++ Controllers/MaskController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MedicalMask.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MaskController : ControllerBase
+    {
+        private readonly MaskService _maskService;
+
+        public MaskController(MaskService maskService)
+        {
+            _maskService = maskService;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var maskCount = await _maskService.GetMaskInfo();
+                return Ok(maskCount);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace MedicalMask
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient<MaskService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
