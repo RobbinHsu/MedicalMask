3e5db6d4b540982ee3283efd4ca98ae8aa7e4f4b
robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-01-06 14:37:47 +0800

    進行 MIGRATION 安裝Entity Framework Core 工具參考

---
+++ Migrations/20210106063109_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedicalMask.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MedicalMasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Tel = table.Column<string>(type: "TEXT", nullable: true),
+                    AdultNum = table.Column<int>(type: "INTEGER", nullable: false),
+                    KidNum = table.Column<int>(type: "INTEGER", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalMasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedicalMasks");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -17,8 +17,36 @@
         # .NET Core
         dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 5.0.1
         ```
-
-
+    * 進行 MIGRATION  
+        如果未安裝**EF Core 命令列工具 (dotnet ef)**，須額外安裝
+        ```csharp
+        # PowerShell
+        Install-Package Microsoft.EntityFrameworkCore.Tools -Version 5.0.1
+        Install-Package Microsoft.EntityFrameworkCore.Design -Version 5.0.1
+        Add-Migration Initial
+        Update-Database
+        # .NET Core
+        dotnet tool install --global dotnet-ef
+        dotnet add package Microsoft.EntityFrameworkCore.Design
+        dotnet ef migrations add Initial
+        dotnet ef database update
+        ```
+        * 驗證安裝
+            ```csharp
+            # PowerShell
+            Get-Help about_EntityFrameworkCore
+            # .NET Core
+            dotnet ef 
+            ```
+            輸出後會看到獨角獸！  
+            ```csharp
+                         _/\__  
+                   ---==/    \\  
+             ___  ___   |.    \|\  
+            | __|| __|  |  )   \\\  
+            | _| | _|   \_/ |  //|\\  
+            |___||_|       /   \\\/\\  
+            ```
 
 ---  
 <a href="https://blog.kkbruce.net/2020/02/ef-core-sqlite.html#.X_LWB9j7SUk" target="_blank">KingKong Bruce記事的 - 簡單五步驟：以EF Core整合SQLite儲存口罩剩餘數量資訊</a>
