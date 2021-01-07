54c250fc4cc51c5493350d164254e24370631f28
robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-01-07 12:10:25 +0800

    如果新資料跟資料庫更新時間相同，就不更新資料庫

--- Controllers/MaskController.cs
+++ Controllers/MaskController.cs
@@ -24,60 +24,13 @@ namespace MedicalMask.Controllers
 
         public async Task<IActionResult> Get()
         {
-            async Task InitialDb(MedicalMask medicalMask)
-            {
-                if (medicalMask == null)
-                {
-                    var maskInfos = await _maskService.GetMaskInfo();
-                    foreach (var maskInfo in maskInfos)
-                    {
-                        _maskContext.Add(new MedicalMask()
-                        {
-                            Code = maskInfo.醫事機構代碼,
-                            Name = maskInfo.醫事機構名稱,
-                            Address = maskInfo.醫事機構地址,
-                            Tel = maskInfo.醫事機構電話,
-                            AdultNum = int.Parse(maskInfo.成人口罩剩餘數),
-                            KidNum = int.Parse(maskInfo.兒童口罩剩餘數),
-                            UpdateTime = DateTime.Parse(maskInfo.來源資料時間),
-                        });
-                    }
-
-                    await _maskContext.SaveChangesAsync();
-                }
-            }
-
-            async Task MaskDataUpdate()
-            {
-                var nextTime = _maskContext.MedicalMasks.First().UpdateTime.AddSeconds(600);
-                var nowTime = DateTime.Now;
-
-                if (nextTime < nowTime)
-                {
-                    _maskContext.RemoveRange(_maskContext.MedicalMasks);
-                    var maskInfos = await _maskService.GetMaskInfo();
-                    foreach (var maskInfo in maskInfos)
-                    {
-                        _maskContext.Add(new MedicalMask()
-                        {
-                            Code = maskInfo.醫事機構代碼,
-                            Name = maskInfo.醫事機構名稱,
-                            Address = maskInfo.醫事機構地址,
-                            Tel = maskInfo.醫事機構電話,
-                            AdultNum = int.Parse(maskInfo.成人口罩剩餘數),
-                            KidNum = int.Parse(maskInfo.兒童口罩剩餘數),
-                            UpdateTime = DateTime.Parse(maskInfo.來源資料時間),
-                        });
-                    }
-                }
-
-                await _maskContext.SaveChangesAsync();
-            }
-
             try
             {
-                var medicalMask = _maskContext.MedicalMasks.FirstOrDefault();
-                await InitialDb(medicalMask);
+                if (IsMedicalMasksNull())
+                {
+                    await InitialDb();
+                }
+
                 await MaskDataUpdate();
 
                 return Ok(_maskContext.MedicalMasks.AsNoTracking());
@@ -86,6 +39,64 @@ namespace MedicalMask.Controllers
             {
                 return Problem(ex.Message);
             }
+        }
+
+        private bool IsMedicalMasksNull()
+        {
+            return _maskContext.MedicalMasks.FirstOrDefault() == null;
+        }
+
+        private async Task InitialDb()
+        {
+            var maskInfos = await _maskService.GetMaskInfo();
+            foreach (var maskInfo in maskInfos)
+            {
+                _maskContext.Add(new MedicalMask()
+                {
+                    Code = maskInfo.醫事機構代碼,
+                    Name = maskInfo.醫事機構名稱,
+                    Address = maskInfo.醫事機構地址,
+                    Tel = maskInfo.醫事機構電話,
+                    AdultNum = int.Parse(maskInfo.成人口罩剩餘數),
+                    KidNum = int.Parse(maskInfo.兒童口罩剩餘數),
+                    UpdateTime = DateTime.Parse(maskInfo.來源資料時間),
+                });
+            }
+
+            await _maskContext.SaveChangesAsync();
+        }
+
+        private async Task MaskDataUpdate()
+        {
+            var updateTime = _maskContext.MedicalMasks.First().UpdateTime;
+            var nextTime = updateTime.AddSeconds(600);
+            var nowTime = DateTime.Now;
+
+            if (nextTime < nowTime)
+            {
+                var maskInfos = await _maskService.GetMaskInfo();
+                var sourceTime = DateTime.Parse(maskInfos.First().來源資料時間);
+
+                if (sourceTime.CompareTo(updateTime) != 0)
+                {
+                    _maskContext.RemoveRange(_maskContext.MedicalMasks);
+                    foreach (var maskInfo in maskInfos)
+                    {
+                        _maskContext.Add(new MedicalMask()
+                        {
+                            Code = maskInfo.醫事機構代碼,
+                            Name = maskInfo.醫事機構名稱,
+                            Address = maskInfo.醫事機構地址,
+                            Tel = maskInfo.醫事機構電話,
+                            AdultNum = int.Parse(maskInfo.成人口罩剩餘數),
+                            KidNum = int.Parse(maskInfo.兒童口罩剩餘數),
+                            UpdateTime = DateTime.Parse(maskInfo.來源資料時間),
+                        });
+                    }
+                }
+            }
+
+            await _maskContext.SaveChangesAsync();
         }
     }
 }
