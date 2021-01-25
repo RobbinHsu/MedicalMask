058b2e287bed5f9b07ae6674314b8686b8f7f5f7
robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-01-25 17:03:45 +0800

    將原本sqlite DB的資料路徑改為專案資料夾底下

--- Model/MaskContext.cs
+++ Model/MaskContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -9,9 +10,13 @@ namespace MedicalMask
     public class MaskContext : DbContext
     {
         public DbSet<MedicalMask> MedicalMasks { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=MedicalMask.db");
+            DirectoryInfo dir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            var directoryInfo = dir.Parent.Parent.Parent;
+            optionsBuilder.UseSqlite($"Data Source={directoryInfo}/MedicalMask.db");
+            //optionsBuilder.UseSqlite("Data Source=MedicalMask.db");
         }
     }
 }
